e2af4e8f5848fc5c197c67ff84115490bc999570
Ben White <bensbigolbeard@gmail.com>
2014-02-22 17:32:07 -0600

    Randomize index list colors. Configure progress bars to reflect percent complete of each list. Ran into problems with progress bar fill animation; not working yet

--- app/assets/javascripts/controllers/ListIndexCtrl.js
+++ app/assets/javascripts/controllers/ListIndexCtrl.js
@@ -2,16 +2,22 @@ angular.module('Todo').controller('ListIndexCtrl', ['$scope', '$location', 'list
 
   $scope.lists = [];
 
-  $scope.showView = false;
+  $scope.showList = false;
+  $scope.showMsg = false;
 
   $scope.determineListView = function(){
     if ($scope.lists.length > 0){
-      $scope.showView = true;
+      $scope.showList = true;
+    } else {
+      $scope.showMsg = true;
     }
   };
 
   listsService.lists(function(data){
     $scope.lists = data;
+    for (var i=0;i<$scope.lists.length;i+=1){
+      $scope.lists[i].percentage = 0;
+    }
     $scope.determineListView();
   });
 
@@ -21,6 +27,34 @@ angular.module('Todo').controller('ListIndexCtrl', ['$scope', '$location', 'list
 
   $scope.createList = function(){
     $location.url('/list/new')
+  };
+  
+  // Calculates completion percentage per list
+
+  $scope.listCompletion = function(index){
+    var checked = 0;
+    var tasks = $scope.lists[index].tasks;
+    for(var i=0;i<tasks.length;i+=1){
+      if (tasks[i].checked === true){
+        checked +=1;
+      }
+    }
+    var percent = Number((checked/tasks.length)*100).toFixed(2);
+    $scope.lists[index].percentage = percent;
+    return Number((checked/tasks.length)*100).toFixed(2);
+    
+  };
+
+  // Selects random color for list
+  $scope.randomizeColor = function(numColors){
+   
+    return Math.floor(Math.random() * (numColors));
+  };
+  
+  $scope.colors = ['aqua','red','blue','green','cyan','purple','pink'];
+
+  $scope.randColor = function(){
+    return $scope.colors[$scope.randomizeColor($scope.colors.length)];
   };
   
 }]);
