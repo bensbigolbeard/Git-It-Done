cde4735b511f05ec8485f6bcfdca833155a21ec5
Ben White <bensbigolbeard@gmail.com>
2014-02-23 21:58:44 -0600

    Replace bg images with versions saved for web to make them a smaller filesize.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   end
 
   def randomized_background_image
-    images = ["assets/1-86417158fad78d0a92ba8ec709b55862.jpg", "assets/2-9f55e694b4ac7e33501ad71b35ad400e.jpg", "assets/3-a3ef37402ece4519a941f7657be8775c.jpg", "assets/4-c5d520d5b2808fce201d18a046c0767e.jpg", "assets/5-fb63264745681dd841fa5917a56b40f9.jpg", "assets/7-9565b5077144453153337acb5d0b5234.jpg", "assets/8-fbec77dc1f26dbd32565bfde4559f3ee.jpg", "assets/9-1a06cf86992c7bc7e4f2e4619484959a.jpg", "assets/10-1156479ea281d527c6dfb3a8fda4894c.jpg"]
+    images = ["assets/1-65b726fd202b510da78a95920cb700a3.jpg", "assets/2-b9fec92317ee123f1063c364874d439d.jpg", "assets/3-475e26ea5bbb6e4855ad4763ba375d0a.jpg", "assets/4-e757a45b9fc69b18057eb93705836486.jpg", "assets/5-aac87a420812511c5e17cea5e0447ba8.jpg", "assets/7-fda3e9b2cdd8ff0e6cb854088597eaf4.jpg", "assets/8-10eee685967f0a54b80dc61ec5fc4908.jpg", "assets/9-486f52e9a6a7e0a2db17969da8a5b0a5.jpg", "assets/10-fae21d9d8d45d816da68b0d5b3a1ef80.jpg"]
     images[rand(images.size)]
   end
 end
